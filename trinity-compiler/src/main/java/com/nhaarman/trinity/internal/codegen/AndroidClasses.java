e07b0b93cb1cbb27a500081dfa6880e45435a073
Niek Haarman <haarman.niek@gmail.com>
2015-02-26 12:03:38 +0100

    Added publishing

--- trinity-compiler/src/main/java/com/nhaarman/trinity/internal/codegen/AndroidClasses.java
+++ trinity-compiler/src/main/java/com/nhaarman/trinity/internal/codegen/AndroidClasses.java
@@ -9,9 +9,7 @@ import com.squareup.javapoet.ClassName;
 public class AndroidClasses {
 
   public static final ClassName CONTENT_VALUES = ClassName.get("android.content", "ContentValues");
-
   public static final ClassName CURSOR = ClassName.get("android.database", "Cursor");
-
   public static final ClassName SQLITE_DATABASE = ClassName.get("android.database.sqlite", "SQLiteDatabase");
 
   private AndroidClasses() {
